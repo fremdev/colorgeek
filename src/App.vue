5f84d8dc25b4cac7a4a90b1f7892534ce30f9703
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-14 17:54:09 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import appHeader from './components/Header';
+
+export default {
+  name: 'app',
+  components: {
+    appHeader,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>ColorGeek</h1>
+    <p>Make your color palette on ColorGeek</p>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+</style>
