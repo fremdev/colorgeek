22a990ffbd432cd66e462ec534b7723cfbac4d83
Alex Efremov <alex.efremov.dev@gmail.com>
2016-12-19 00:09:50 +0200

    Add navbar with 2 router-links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>
-    <h1>ColorGeek</h1>
-    <p>Make your color palette on ColorGeek</p>
+  <div class="header">
+    <div class="logo">
+      <router-link class="logo__title" tag="h1" to="/">ColorGeek</router-link>
+      <p>Make your color palette on ColorGeek</p>
+    </div>
+    <nav-bar></nav-bar>
   </div>
+
 </template>
 
 <script>
+import NavBar from './NavBar';
+
 export default {
+  components: {
+    NavBar,
+  },
 };
 </script>
 
 <style scoped>
+  .header {
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .logo__title {
+    cursor: pointer;
+    color: #5454c5;
+  }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="navigation">
+    <router-link
+      to="/auth/register"
+      class="menu-item"
+      activeClass="menu-item--active"
+    >Sign Up</router-link>
+    <router-link
+      to="/auth/login"
+      class="menu-item"
+      activeClass="menu-item--active"
+    >Sign In</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+  .navigation {
+    padding: 10px 20px;
+  }
+  .menu-item {
+    margin-right: 10px;
+  }
+  .menu-item--active {
+    background-color: #9999ee;
+    color: #fff;
+  }
+</style>
